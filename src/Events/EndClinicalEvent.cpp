0976f67d2bc40c71636e9341f6dd6da4cfcbc9b2
KienTTran <kientt@temple.edu>
2025-12-24 18:19:12 -0500

    Disable recurrent and in EndClinicalEvent

--- src/Events/EndClinicalEvent.cpp
+++ src/Events/EndClinicalEvent.cpp
@@ -31,18 +31,18 @@ void EndClinicalEvent::do_execute() {
 
     person->determine_relapse_or_not(clinical_caused_parasite_);
 
-    if (person->get_all_clonal_parasite_populations()->contain(
-            clinical_caused_parasite_)) {
-      auto log_parasite_density =
-          clinical_caused_parasite_->last_update_log10_parasite_density();
-
-      // becase the current model does not have within host dynamics, so we
-      // assume that the threshold for the parasite density to re-appear in
-      // blood is 100 per uL
-      const bool isHigherThanRecrudescenceThreshold = log_parasite_density > 2;
-      if (isHigherThanRecrudescenceThreshold) {
-        person->determine_symptomatic_recrudescence(clinical_caused_parasite_);
-      }
-    }
+    // if (person->get_all_clonal_parasite_populations()->contain(
+    //         clinical_caused_parasite_)) {
+    //   auto log_parasite_density =
+    //       clinical_caused_parasite_->last_update_log10_parasite_density();
+    //
+    //   // becase the current model does not have within host dynamics, so we
+    //   // assume that the threshold for the parasite density to re-appear in
+    //   // blood is 100 per uL
+    //   const bool isHigherThanRecrudescenceThreshold = log_parasite_density > 2;
+    //   if (isHigherThanRecrudescenceThreshold) {
+    //     person->determine_symptomatic_recrudescence(clinical_caused_parasite_);
+    //   }
+    // }
   }
 }
