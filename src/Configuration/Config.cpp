fd5ab01f9792af0532960ef6656078ba32c0fd95
KienTTran <86167342+KienTTran@users.noreply.github.com>
2026-09-18 14:58:48 -0400

    Revert "Implementation of SMC feature and Daily SMC Reporter for v6 model of simulation"

--- src/Configuration/Config.cpp
+++ src/Configuration/Config.cpp
@@ -57,7 +57,6 @@ void Config::reset_load_state() {
   version6_pfpr_incidence_calibrations_ = ImmuneSystemParameterOverrides{};
   has_version6_pfpr_incidence_calibrations_ = false;
   population_events_ = PopulationEvents{};
-  //smc_reporter_settings_ = SMCReporterSettings{}; //SMCReporterSettings
 }
 
 void Config::parse_configuration(const YAML::Node &config) {
@@ -82,9 +81,9 @@ void Config::parse_configuration(const YAML::Node &config) {
   YAML::convert<EpidemiologicalParameters>::decode(config["epidemiological_parameters"], epidemiological_parameters_);
   YAML::convert<MosquitoParameters>::decode(config["mosquito_parameters"], mosquito_parameters_);
 
-  YAML::convert<RaptSettings>::decode(config["rapt_settings"], rapt_settings_);
-  YAML::convert<SMCReporterSettings>::decode(config["smc_reporter_settings"], smc_reporter_settings_); //SMCReporterSettings
-
+  if (config["rapt_settings"]) {
+    YAML::convert<RaptSettings>::decode(config["rapt_settings"], rapt_settings_);
+  }
 }
 
 void Config::parse_version6_pfpr_incidence_calibrations(const YAML::Node &config) {
@@ -934,16 +933,10 @@ void Config::validate_population_events() const {
     if (population_event.get_name().empty()) {
       throw std::invalid_argument("Name should be provided for all population events");
     }
-    if(population_event.get_name()=="SMC") { return;} // event date is later constructed based on year range 
     for (auto event_info : population_event.get_info()) {
       // Check if event date is valid
       if (event_info.get_date() < simulation_timeframe_.get_starting_date()
           || event_info.get_date() > simulation_timeframe_.get_ending_date()) {
-spdlog::error(
-    "Event date {} is not in between simulation starting date {} and ending date {}",
-    date::format("%F", event_info.get_date()),
-    date::format("%F", simulation_timeframe_.get_starting_date()),
-    date::format("%F", simulation_timeframe_.get_ending_date()));
         throw std::invalid_argument(
             "Event date should be in between simulation starting and ending date");
       }
--- src/Reporters/Reporter.h
+++ src/Reporters/Reporter.h
@@ -56,7 +56,6 @@ public:
     SEASONAL_IMMUNITY,
     AGE_BAND_REPORTER,
     THERAPY_RECORD_REPORTER,
-    SMC_REPORTER, // SMC Reporter
 
     // SQLite reporter
     // SQLITE_DISTRICT_REPORTER,
