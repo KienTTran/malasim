df13895cdcbb5d48b9e2c65bea74cf3040579f33
sarit-adh <sarit.adhikari@gmail.com>
2026-09-23 16:14:13 -0400

    Applied pfpr6to17 reporting patch

--- src/Reporters/SQLiteMonthlyReporter.h
+++ src/Reporters/SQLiteMonthlyReporter.h
@@ -29,7 +29,7 @@ protected:
   bool enable_cell_level_reporting{false};
 
   struct MonthlySiteData {
-    std::vector<double> eir, pfpr_under5, pfpr2to10, pfpr_all;
+    std::vector<double> eir, pfpr_under5, pfpr2to10, pfpr6to17, pfpr_all;
     std::vector<int> population, clinical_episodes, treatments, treatment_failures, nontreatment,
         treatments_under5, treatments_over5, infections_by_unit;
     std::vector<std::vector<int>> clinical_episodes_by_age_class;
